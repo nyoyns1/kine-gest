a2bac1292ba27970e5ad163f52d673f4d72f3eb1
nyoyns1 <152090534+nyoyns1@users.noreply.github.com>
2026-03-06 00:05:47 +0000

    Update server.ts

--- server.ts
+++ server.ts
@@ -35,7 +35,7 @@ const checkRole = (allowedRoles: UserRole[]) => {
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = parseInt(process.env.PORT || "3000");
 
   app.use(express.json());
 
